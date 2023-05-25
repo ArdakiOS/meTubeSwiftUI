7a9e965e5aa88f5361ab01fbb36f4206cb8c9710
Ardak <luxr2ge@gmail.com>
2023-05-25 16:29:51 +0600

    Initial Commit

---
+++ meTubeSwiftUI/meTubeSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  meTubeSwiftUIApp.swift
+//  meTubeSwiftUI
+//
+//  Created by Ardak Tursunbayev on 25.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct meTubeSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
